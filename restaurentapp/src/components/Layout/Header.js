896fb0bcc195d6f57db865d83790a7b4c3d8966c
ruchitakb7 <ruchitakb7@gmail.com>
2024-10-11 13:31:09 +0530

    summary

--- restaurentapp/src/components/Layout/Header.js
+++ restaurentapp/src/components/Layout/Header.js
@@ -7,12 +7,22 @@ const Header=()=>{
            <div>
              <header className={classes.header}>
                 <h1>ReactMeals</h1>
-               <button>Cart</button>
-             </header>    
+               <button>Cart:{0}</button>
+             </header> 
+               
              <div className={classes['main-image']}>
                 <img src={Food_header}
                 alt=""></img>
              </div>
+            <div className={classes.summary}>
+              <h2>Delicious Food Delivery To You</h2>
+              <p>Choose your favourite meal from our brand selection of available meals and enjoy a delicious lunch
+                or dinner at home
+              </p>
+              <p>
+                All our food cooks with high quality ingredients and of course just in time and experienced chefs!
+              </p>
+              </div> 
            </div> 
     )
 }
