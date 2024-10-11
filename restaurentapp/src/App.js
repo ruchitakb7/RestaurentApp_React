3b6867a453aa8437735bdead40ed62510b3a7a51
ruchitakb7 <ruchitakb7@gmail.com>
2024-10-12 03:13:52 +0530

    git commit -m "cart"

--- restaurentapp/src/App.js
+++ restaurentapp/src/App.js
@@ -2,12 +2,14 @@ import { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header.js';
 import Meals from './components/Meals/Meals.js';
-
+import CartItem from './components/Cart/CartItem.js';
 function App() {
   return (
     <Fragment>
+      
   <Header/>
   <Meals></Meals>
+  <CartItem></CartItem>
   </Fragment>
   );
 }
